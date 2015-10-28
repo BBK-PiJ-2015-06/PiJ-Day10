329184d63153abb711249f54f73c7e58019298b3
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-28 19:42:59 +0000

    Simple Even class created to extend from String. Error when compiling as String is a final class.

---
+++ Exercise 7 - Final means no change/Even.java
@@ -0,0 +1,12 @@
+public class Even extends String {
+	
+	public void printEven() {
+		System.out.println("Even numbers only");
+		//Method to print only even numbers from string input.
+	}
+	
+}
+
+/**
+* Creates error "cannot inherit from final String"
+*/
