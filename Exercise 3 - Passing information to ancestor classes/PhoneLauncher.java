ac602d5f7941c54c53ebc1d9afb7f886e09acd93
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-28 14:58:16 +0000

    PhoneLauncher test script extended to test all subclasses.

--- Exercise 3 - Passing information to ancestor classes/PhoneLauncher.java
+++ Exercise 3 - Passing information to ancestor classes/PhoneLauncher.java
@@ -1,10 +1,12 @@
 public class PhoneLauncher {
+	
 	public static void main(String[] args) {
 		PhoneLauncher launcher = new PhoneLauncher();
 		launcher.launch();
 	}
+	
 	public void launch() {
-		SmartPhone myphone = new SmartPhone("Sony");
+		SmartPhone myphone = new SmartPhone("brand");
 		myphone.call("07985970880");
 		myphone.call("00483565929");
 		myphone.ringAlarm("Wake Up!");
@@ -13,5 +15,23 @@ public class PhoneLauncher {
 		myphone.browseWeb("Google");
 		myphone.findPosition();
 		System.out.println(myphone.getBrand());
+		
+		System.out.println("");
+		
+		MobilePhone myMobile = new MobilePhone("brand");
+		myMobile.call("07985970880");
+		myMobile.call("00483565929");
+		myMobile.ringAlarm("Wake Up!");
+		myMobile.playGame("Minesweeper");
+		myMobile.printLastNumbers();
+		System.out.println(myMobile.getBrand());
+		
+		System.out.println("");
+		
+		OldPhone myOld = new OldPhone("brand");
+		myOld.call("07985970880");
+		myOld.call("00483565929");
+		System.out.println(myOld.getBrand());
 	}
+	
 }
