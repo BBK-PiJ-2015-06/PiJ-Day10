6becae078cc2efa3d7f57b1d733cc699a9f3e3f1
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-28 16:42:26 +0000

    Method playGame(String) changed from public to private. Test confirms program no longer compiles.

--- Exercise 4 - Visibility/MobilePhone.java
+++ Exercise 4 - Visibility/MobilePhone.java
@@ -1,7 +1,41 @@
 public class MobilePhone extends OldPhone {
 	
+	private String[] recentCalls = {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"};
+	
 	public MobilePhone(String brand) {
 		super(brand);
+	}
+	
+	@Override
+	public void call(String number) {
+		super.call(number);
+		populateRecentCalls(number);
+	} 
+	
+	public void populateRecentCalls(String number) {
+		int count = 0;
+		if(recentCalls[count].equals("-")) {
+			recentCalls[count] = number;
+		} else if(recentCalls[9].equals("-")) {
+			while(recentCalls[count] != "-") {
+				count++;
+				if(count > 9) {
+					return;
+				}
+			}
+			recentCalls[count] = number;
+		} else {
+			recentCalls[0] = recentCalls[1];
+			recentCalls[1] = recentCalls[2];
+			recentCalls[2] = recentCalls[3];
+			recentCalls[3] = recentCalls[4];
+			recentCalls[4] = recentCalls[5];
+			recentCalls[5] = recentCalls[6];
+			recentCalls[6] = recentCalls[7];
+			recentCalls[7] = recentCalls[8];
+			recentCalls[8] = recentCalls[9];
+			recentCalls[9] = number;
+		}
 	}
 	
 	public void ringAlarm(String event) {
@@ -13,17 +47,12 @@ public class MobilePhone extends OldPhone {
 	}
 	
 	public void printLastNumbers() {
-		System.out.println("Recent contacts: ");
-		System.out.println("07985970880");
-		System.out.println("01483565929");
-		System.out.println("07918088592");
-		System.out.println("01483565929");
-		System.out.println("01483888000");
-		System.out.println("07985970880");
-		System.out.println("01483565929");
-		System.out.println("07918088592");
-		System.out.println("01483565929");
-		System.out.println("01483888000");
+		System.out.println("Recent contacts:");
+		for(int i = 9; i >= 0; i--) {
+			if(recentCalls[i] != "-") {
+				System.out.println(recentCalls[i]);
+			}
+		}
 	}
 	
 }
