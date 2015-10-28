e6570eaadf15783b0afba54f959806b27940a80d
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-28 11:46:25 +0000

    Class SmartPhone.java created as extends from MobilePhone.

---
+++ Exercise 1 - Extension, extension/SmartPhone.java
@@ -0,0 +1,15 @@
+public class SmartPhone extends MobilePhone {
+	
+	public Smartphone() {
+		super();
+	}
+	
+	public void browseWeb(String website) {
+		System.out.println("Browsing to " + website);
+	}
+	
+	public int findPosition() {
+		System.out.println("GPS-found position: 9832767032735");
+	}
+	
+}
