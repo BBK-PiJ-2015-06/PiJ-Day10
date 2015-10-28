eaaadcbdb18f49d5b1685ac821f6fbaf31e3e9c4
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-28 19:04:17 +0000

    New class Guiter created, and made to implement WoodenObject and Musicalnstrument interfaces.

---
+++ Exercise 5 - Multiple Inheritance/Guitar.java
@@ -0,0 +1,12 @@
+public class Guitar implements MusicalInstrument, WoodenObject {
+	
+	public void play() {
+		System.out.println("Playing...");
+	}
+	
+	
+	public void burn() {
+		System.out.println("Burning...");
+	}
+	
+}
